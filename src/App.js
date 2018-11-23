21afee751bad32c2e0c6368eab31a1cdb0ca1fbc
EunH <whitexo@naver.com>
2018-11-23 23:06:24 +0900

    package.json 변경

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const Title = ({tit}) => {
+  return (
+    <div className="up_tit">{tit}</div>
+  );
+};
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-      test
+        <Title tit='Portfolio'/>
       </div>
     );
   }
